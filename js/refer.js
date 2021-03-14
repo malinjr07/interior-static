6b1dab7b858a90dced33492ab9095aa1ca7219a6
Mosaddik Ali Maruf <malinjr07@gmail.com>
2021-03-14 22:14:29 +0600

    dumb file removed

--- js/refer.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  var checkBox = document.getElementById("myCheck");
-  var butto = document.getElementById("button");
-  if (checkBox.checked == true){
-    butto.style.display = "block";
-  } else {
-     butto.style.display = "none";
-  }
-}
